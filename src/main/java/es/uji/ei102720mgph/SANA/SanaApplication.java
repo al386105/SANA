8f2f1d0d8af60ece0bfe1db9cf8bad50203a002b
al386105 <al386105@uji.es>
2021-03-23 12:37:49 +0100

    first commit

---
+++ src/main/java/es/uji/ei102720mgph/SANA/SanaApplication.java
@@ -0,0 +1,13 @@
+package es.uji.ei102720mgph.SANA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SanaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SanaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/es/uji/ei102720mgph/SANA/SanaApplicationTests.java
@@ -0,0 +1,13 @@
+package es.uji.ei102720mgph.SANA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SanaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

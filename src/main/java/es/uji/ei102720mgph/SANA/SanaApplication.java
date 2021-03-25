1b5ea25906992aaa5bb9bf6758bca79f83a2b47d
al386013 <al386013@uji.es>
2021-03-25 17:23:00 +0100

    SanaApplication y SanaConfiguration

--- src/main/java/es/uji/ei102720mgph/SANA/SanaApplication.java
+++ src/main/java/es/uji/ei102720mgph/SANA/SanaApplication.java
@@ -2,9 +2,14 @@ package es.uji.ei102720mgph.SANA;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.util.logging.Logger;
 
+//L'anotació permet que l'app s'autoconfigure, que puga buscar components
+//i que es puguen configurar dependències entre ells
 @SpringBootApplication
 public class SanaApplication {
+
+	private static final Logger log = Logger.getLogger(SanaApplication.class.getName());
 
 	public static void main(String[] args) {
 		SpringApplication.run(SanaApplication.class, args);
---
+++ src/main/java/es/uji/ei102720mgph/SANA/SanaConfiguration.java
@@ -0,0 +1,22 @@
+package es.uji.ei102720mgph.SANA;
+
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+// Per a configurar el DataSource i alguns casos de injecció de dependències
+
+@Configuration
+public class SanaConfiguration {
+    // Configura l'accés a la base de dades (DataSource)
+    // a partir de les propietats a src/main/resources/applications.properties
+    // que comencen pel prefix spring.datasource
+    @Bean
+    @ConfigurationProperties(prefix = "spring.datasource")
+    public DataSource dataSource() {
+        return DataSourceBuilder.create().build();
+    }
+}

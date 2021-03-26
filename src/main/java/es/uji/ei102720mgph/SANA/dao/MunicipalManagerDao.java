62b5e60afd350f2a04d7455258c3543dd60b383c
Carlos <al386007@uji.es>
2021-03-26 23:17:54 +0100

    Primero de Carlos

--- src/main/java/es/uji/ei102720mgph/SANA/dao/MunicipalManagerDao.java
+++ src/main/java/es/uji/ei102720mgph/SANA/dao/MunicipalManagerDao.java
@@ -1,4 +1,31 @@
 package es.uji.ei102720mgph.SANA.dao;
 
+
+import es.uji.ei102720mgph.SANA.model.MunicipalManager;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import javax.sql.DataSource;
+
+@Repository
 public class MunicipalManagerDao {
+
+    private JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public void setDataSource(DataSource dataSource) {
+        this.jdbcTemplate = new JdbcTemplate(dataSource);
+    }
+
+    public void addMunicipalManager(MunicipalManager manager) {
+        jdbcTemplate.update(
+                "INSERT INTO MunicipalManager VALUES(?, ?, ?, ?)",
+                manager.getEmail(), manager.getUsername(), manager.getPassword(), manager.getMunicipality());
+
+    }
+
+    public void deleteMunicipalManager(String email) {
+        jdbcTemplate.update("DELETE FROM MunicipalManager WHERE email =?", email);
+    }
 }
--- src/main/java/es/uji/ei102720mgph/SANA/model/MunicipalManager.java
+++ src/main/java/es/uji/ei102720mgph/SANA/model/MunicipalManager.java
@@ -1,4 +1,52 @@
 package es.uji.ei102720mgph.SANA.model;
 
 public class MunicipalManager {
+    private String email;
+    private String username;
+    private String password;
+    private String municipality;
+
+    public MunicipalManager(){}
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getMunicipality() {
+        return municipality;
+    }
+
+    public void setMunicipality(String municipality) {
+        this.municipality = municipality;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "MunicipalManager{" +
+                "email=" + email +
+                ", usuario=" + username +
+                ", password=" + password +
+                ", municipality=" + municipality +
+                '}';
+    }
 }

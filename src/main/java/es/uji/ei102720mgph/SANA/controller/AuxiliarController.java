5ea5d3b266381acb4d0935275f9033bf81567bb4
Carlos <al386007@uji.es>
2021-04-28 22:45:17 +0200

    Funciona botón y formulario del contactanos, Carlos

--- src/main/java/es/uji/ei102720mgph/SANA/controller/AuxiliarController.java
+++ src/main/java/es/uji/ei102720mgph/SANA/controller/AuxiliarController.java
@@ -14,22 +14,22 @@ public class AuxiliarController {
 
     @RequestMapping("inicio")
     public String redirigirSana(Model model) {
-        //model.addAttribute("email", new Email());
+        model.addAttribute("email", new Email());
         return "inicio/sana";
     }
 
-    @RequestMapping(value="/add", method= RequestMethod.POST)
+    @RequestMapping(value="/enviarCorreo", method=RequestMethod.POST)
     public String processAddSubmit(@ModelAttribute("email") Email email,
                                    BindingResult bindingResult) {
         if (bindingResult.hasErrors())
-            return "email/add"; //tornem al formulari per a que el corregisca
-        //emailDao.addEmail(email); //usem el dao per a inserir el email
+            return "inicio/sana"; //tornem al formulari per a que el corregisca
+
         System.out.println("ID: " + email.getId());
         System.out.println("Sender: " + email.getSender());
         System.out.println("Date: " + email.getDate());
         System.out.println("SanaUser: " + email.getSanaUser());
         System.out.println("Subject: " + email.getSubject());
         System.out.println("Body: " + email.getTextBody());
-        return "redirect:list"; //redirigim a la lista per a veure el email afegit, post/redirect/get
+        return "redirect:inicio/sana"; //redirigim a la lista per a veure el email afegit, post/redirect/get
     }
 }

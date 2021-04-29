f1d0725050090e87002939342cdaf068bd5abeee
Carlos <al386007@uji.es>
2021-04-29 09:26:12 +0200

    Ya envía mails en el contactanos, Carlos

--- src/main/java/es/uji/ei102720mgph/SANA/controller/AuxiliarController.java
+++ src/main/java/es/uji/ei102720mgph/SANA/controller/AuxiliarController.java
@@ -8,6 +8,11 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import javax.mail.*;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import java.util.Properties;
+
 @Controller
 @RequestMapping("/")
 public class AuxiliarController {
@@ -24,12 +29,45 @@ public class AuxiliarController {
         if (bindingResult.hasErrors())
             return "inicio/sana"; //tornem al formulari per a que el corregisca
 
-        System.out.println("ID: " + email.getId());
-        System.out.println("Sender: " + email.getSender());
-        System.out.println("Date: " + email.getDate());
-        System.out.println("SanaUser: " + email.getSanaUser());
-        System.out.println("Subject: " + email.getSubject());
-        System.out.println("Body: " + email.getTextBody());
-        return "redirect:inicio/sana"; //redirigim a la lista per a veure el email afegit, post/redirect/get
+        String destinatario = email.getSanaUser();
+        String asunto = email.getSubject();
+        String cuerpo = email.getTextBody();
+
+        // Envia correo electrónico
+        enviarMail(destinatario, asunto, cuerpo);
+
+        return "redirect:inicio"; //redirigim a la lista per a veure el email afegit, post/redirect/get
+    }
+
+
+    public void enviarMail(String destinatario, String asunto, String cuerpo) {
+        String remitente = "sana.espais.naturals";  //Para la dirección nomcuenta@gmail.com
+
+        Properties props = System.getProperties();
+        props.put("mail.smtp.host", "smtp.gmail.com");  //El servidor SMTP de Google
+        props.put("mail.smtp.user", remitente);
+        props.put("mail.smtp.clave", "barrachina");    //La clave de la cuenta
+        props.put("mail.smtp.auth", "true");    //Usar autenticación mediante usuario y clave
+        props.put("mail.smtp.starttls.enable", "true"); //Para conectar de manera segura al servidor SMTP
+        props.put("mail.smtp.port", "587"); //El puerto SMTP seguro de Google
+
+        Session session = Session.getDefaultInstance(props);
+        MimeMessage message = new MimeMessage(session);
+
+
+        try {
+            message.setFrom(new InternetAddress(remitente));
+
+            message.addRecipient(Message.RecipientType.TO, new InternetAddress(destinatario));   //Se podrían añadir varios de la misma manera
+            message.setSubject(asunto);
+            message.setText(cuerpo);
+            Transport transport = session.getTransport("smtp");
+            transport.connect("smtp.gmail.com", remitente, "barrachina");
+            transport.sendMessage(message, message.getAllRecipients());
+            transport.close();
+        }
+        catch (MessagingException me) {
+            me.printStackTrace();   //Si se produce un error
+        }
     }
 }
